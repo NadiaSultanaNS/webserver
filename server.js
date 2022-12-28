ebf94ee97ab94ea5bd228c01bfd687a5274f1b82
Nadia Sultana <nadiasultana8899@gmail.com>
2022-12-28 19:11:42 +0600

    project completed

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require('express')
 const path = require('path')
+const bodyParser = require('body-parser');
 const app = express()
 const port = 3000
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/static/index.html'))
+})
+
+app.get('/nadia', (req, res) => {
+    res.sendFile(path.join(__dirname, '/static/nadia.html'))
+})
+
+app.post('/comment', (req, res) => {
+    console.log(req.body)
+    res.send(req.body)
 })
 
 app.listen(port, () => {
